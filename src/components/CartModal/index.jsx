00726ad7eb16fe07045d2965c9ef83f9d3ac3426
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-18 14:11:28 -0300

    fechar modal clicando fora e com esc

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,8 +1,11 @@
 import { MdClose } from "react-icons/md";
 import { CartItemCard } from "./CartItemCard";
-import { ProductCard } from "../ProductList/ProductCard";
+import { StyledDiv } from "./style";
+import { useEffect, useRef } from "react";
 
 export const CartModal = ({ productList, cartList, setCartList, setIsOpen }) => {
+   const modalRef = useRef(null);
+
    const total = cartList.reduce((prevValue, product) => {
       return prevValue + product.price;
    }, 0);
@@ -16,34 +19,58 @@ export const CartModal = ({ productList, cartList, setCartList, setIsOpen }) => 
       }
    }
 
+   const handleOutclick = (event) => {
+      if(!modalRef.current.contains(event.target)){
+         setIsOpen(false)
+      }
+   }
+
+   const handleKey = (event) => {
+      if(event.key === "Escape"){
+         setIsOpen(false)
+      }
+   }
+
+   useEffect(() => {
+      window.addEventListener("mousedown", handleOutclick)
+      window.addEventListener("keydown", handleKey)
+
+      return () => {
+         window.removeEventListener("mousedown", handleOutclick)
+         window.removeEventListener("keydown", handleKey)
+      }
+   }, [])
+
    return (
-      <div role="dialog">
-         <div>
-            <h2>Carrinho de compras</h2>
-            <button onClick={() => setIsOpen(false)} aria-label="close" title="Fechar">
-               <MdClose size={21} />
-            </button>
-         </div>
-         <div>
-            {
-               cartList.length ===0 ?
-               <p>Você ainda não possui nenhum produto no carrinho</p> :
-               (
-                  <ul>
-                     {productList.map((product) => (
-                        cartFunction(product)
-                     ))}
-                  </ul>
-               )
-            }
-         </div>
-         <div>
+      <StyledDiv role="dialog">
+         <div ref={modalRef}>
             <div>
-               <span>Total</span>
-               <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</span>
+               <h2>Carrinho de compras</h2>
+               <button onClick={() => setIsOpen(false)} aria-label="close" title="Fechar">
+                  <MdClose size={21} />
+               </button>
             </div>
-            <button onClick={() => setCartList([])}>Remover todos</button>
+            <div>
+               {
+                  cartList.length === 0 ?
+                     <p>Você ainda não possui nenhum produto no carrinho</p> :
+                     (
+                        <ul>
+                           {productList.map((product) => (
+                              cartFunction(product)
+                           ))}
+                        </ul>
+                     )
+               }
+            </div>
+            <div>
+               <div>
+                  <span>Total</span>
+                  <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</span>
+               </div>
+               <button onClick={() => setCartList([])}>Remover todos</button>
+            </div>
          </div>
-      </div>
+      </StyledDiv>
    );
 };
